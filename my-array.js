8f2eb4e79ea93eaf2eacc503b0484aecb61ccf5b
kie <jutatipr97@gmail.com>
2026-07-02 23:03:52 +0700

    edied array

--- my-array.js
+++ my-array.js
@@ -1,3 +1,2 @@
-let cities = ["Seoul" , "Tokyo" , "Bangkok", "Pattaya" , "Khon Kaen" , "Udon Thani" , "Chiang Mai" , "Chiang Rai" , "Phuket" , "Hua Hin"];
-
+let cities = ["Seoul", "Tokyo", "Bangkok", "Pattaya", "Khon Kaen", "Udon Thani", "Chiang Mai", "Chiang Rai", "Phuket"];
 console.log(cities);
